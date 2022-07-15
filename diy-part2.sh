2b56efd93f7b2859ba525eb50953f8d6046a5fca
kwokwai6618 <58355825+kwokwai6618@users.noreply.github.com>
2022-07-15 10:28:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,18 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 
+# Add shadowsocks-rust 1.15.0-alpha.4
+rm -rf package/helloworld/shadowsocks-rust
+svn co  https://github.com/Mattaclp/NewLEDE/trunk/shadowsocks-rust package/helloworld/shadowsocks-rust
+
+# Add NanoHatOLED weather support (tested not work, need to fix)
+#rm -rf feeds/NanoHatOLED/nanohatoled/files/NanoHatOLED/bakebit_nanohat_oled.py
+#wget -O Zpix.ttf https://github.com/vinewx/NanoHatOLED/raw/weather/nanohatoled/files/NanoHatOLED/Zpix.ttf
+#wget -O bakebit_nanohat_oled.py https://github.com/vinewx/NanoHatOLED/raw/weather/nanohatoled/files/NanoHatOLED/bakebit_nanohat_oled.py
+#sed -i 's/101010100/101210401/g' bakebit_nanohat_oled.py
+#mv Zpix.ttf feeds/NanoHatOLED/nanohatoled/files/NanoHatOLED/
+#mv bakebit_nanohat_oled.py feeds/NanoHatOLED/nanohatoled/files/NanoHatOLED/
+
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 #rm -rf feeds/luci/applications/luci-app-jd-dailybonus
@@ -28,7 +40,7 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' pa
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改内核版本
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.18/g' target/linux/x86/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/x86/Makefile
 #sed -i 's/KERNEL_PATCHVER=5.10/KERNEL_PATCHVER=5.15/g' target/linux/rockchip/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER=5.15/KERNEL_TESTING_PATCHVER=5.4/g' target/linux/rockchip/Makefile
